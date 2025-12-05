8667c2c3bf2aa90745a359a6e2ea2b26912ec1cb
Florian-Heringa <florian_music@hotmail.com>
2025-12-05 23:22:11 +0000

    finished challenge

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
+use regex::Regex;
+
 mod run_length_encoding {
+    use regex::Regex;
+
     pub fn encode(text: &str) -> String {
-        todo!()
+
+        let mut res = String::new();
+        let mut count: usize = 1;
+        let mut prev = text.chars().next().unwrap();
+        
+        for c in text.chars().skip(1) {
+            if c == prev && count < 9 {
+                count += 1;
+            } else {
+                res.push_str(&count.to_string());
+                res.push(prev);
+                prev = c;
+                count = 1;
+            }
+        }
+        // Handle final character
+        res.push_str(&count.to_string());
+        res.push(prev);
+        res
     }
     
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut result = String::new();
+        let re = Regex::new(r"(?<amount>\d)(?<character>[\d\w\s])").unwrap();
+        for caps in re.captures_iter(text) {
+            result.push_str(
+                &[caps.name("character").unwrap().as_str()]
+                    .repeat(caps.name("amount").unwrap().as_str().parse::<usize>().unwrap())
+                    .join("")
+                )
+        }
+        result
     }
 }
 
 fn main() {
-    // 
+    use run_length_encoding::*;
+    let s = "5A1 9A1A1 9A9A2A";
+    println!("{:?}", decode(s));
 }
 
 #[test]
