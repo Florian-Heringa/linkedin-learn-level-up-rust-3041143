e925adf84097f84bc0e9f6c9333cd66aa6b4a088
Florian-Heringa <florian_music@hotmail.com>
2025-12-05 21:53:12 +0000

    finished exercise

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+struct Rgb(u8, u8, u8);
+
+#[derive(Debug)]
+enum RgbParseErr {
+    IncorrectLength,
+    //InvalidHexString,
+    NoLeadingHash,
+    RedChannelOutOfBounds,
+    GreenChannelOutOfBounds,
+    BlueChannelOutOfBounds,
+}
 
 trait RgbChannels {
     fn r(&self) -> u8;
@@ -14,10 +24,38 @@ trait RgbChannels {
 
 impl RgbChannels for Rgb {
     // TODO: implement trait
+    fn b(&self) -> u8 {
+        self.2
+    }
+
+    fn g(&self) -> u8 {
+        self.1
+    }
+
+    fn r(&self) -> u8 {
+        self.0
+    }
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+    type Err = RgbParseErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() != 7 {
+            return Err(RgbParseErr::IncorrectLength);
+        };
+        if let Some(hex) = s.strip_prefix('#') {
+            let r = u8::from_str_radix(&hex[0..2], 16)
+                .map_err(|_| RgbParseErr::RedChannelOutOfBounds)?;
+            let g = u8::from_str_radix(&hex[2..4], 16)
+                .map_err(|_| RgbParseErr::GreenChannelOutOfBounds)?;
+            let b = u8::from_str_radix(&hex[4..6], 16)
+                .map_err(|_| RgbParseErr::BlueChannelOutOfBounds)?;
+            Ok(Rgb(r, g, b))
+        } else {
+            Err(RgbParseErr::NoLeadingHash)
+        }
+    }
 }
 
 impl Display for Rgb {
@@ -26,9 +64,7 @@ impl Display for Rgb {
     }
 }
 
-fn main() {
-    // 
-}
+fn main() {}
 
 #[test]
 fn every_color() {
@@ -43,19 +79,19 @@ fn every_color() {
 
 #[test]
 #[should_panic]
-fn too_short () {
+fn too_short() {
     let _: Rgb = "1234".parse().unwrap();
 }
 
 #[test]
 #[should_panic]
-fn not_a_hex_code () {
+fn not_a_hex_code() {
     let _: Rgb = "?".parse().unwrap();
 }
 
 #[test]
 #[should_panic]
-fn invalid_literals () {
+fn invalid_literals() {
     let _: Rgb = "?".parse().unwrap();
 }
 
@@ -70,4 +106,3 @@ fn no_leading_hash() {
 fn out_of_bounds() {
     let _: Rgb = "00gg00".parse().unwrap();
 }
-
